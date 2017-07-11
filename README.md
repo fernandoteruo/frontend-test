661eac4e21b6eb7c11d17286bcc7f03d345bb747
fernandoteruo <fernandoteruo@gmail.com>
2017-07-11 02:16:04 -0300

    project architecture, set-up gulp, project run

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
-# Netflix Roulette
+# Netflix Roulette - version ng1
 
 ## Summary
+Netflix roulette based on angular - ng1 (v1.0)
+
 ## Technologies, frameworks and libraries
-## Development and Contribution
-## Deploy
-## Testing
+Html, javascript, css, sass, angularjs, bootstrap3, gulp, git
+
+## Building and running
+If you want to build and run this project follow the following bellow
+
+### Pre-requisites
+- Install yarn dependencies
+
+### Terminal Commands
+- 'gulp start': build and run the app with development configuration, you can also pass options environment options (--env test or --env prod)
+
+## Future (TODO)
+- 'gulp tests': run unit testing
+- 'gulp e2e': run e2e testing
+- ReactJs migration
+
 ## Author
-
-
-
-
-
-
-
+fernandoteruo@gmail.com
 
 
 
---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+(function() {
+	"use strict";
+
+	var gulp = require("gulp");
+	var sass = require("gulp-sass");
+	var rename = require("gulp-rename");
+	var del = require("del");
+	var minimist = require("minimist");
+	var shell = require("gulp-shell");
+	var flatten = require("gulp-flatten");
+	var jsMin = require("gulp-uglify");
+	var htmlMin = require("gulp-htmlmin");
+	var util = require("gulp-util");
+	var merge = require("merge-stream");
+	var replace = require("gulp-replace");
+
+	var srcRoot = "src/";
+	var knownOptions = {
+		string: ["env"],
+		"default": {
+			env: "dev"
+		}
+	};
+	var options = minimist(process.argv.slice(2), knownOptions);
+	var dstPath = "build/" + options.env + "/";
+
+	function clean() {
+		return del([dstPath + "**/*.*"]);
+	}
+	var renameOptions = {
+		dirname: "",
+		suffix: ".min"
+	};
+
+	function sassCompileMinify() {
+		var sassOptions = {
+			outputStyle: "compressed",
+			precision: 8
+		};
+		return gulp.src([srcRoot + "sass/**/*.scss"]).pipe(sass(sassOptions).on("error", sass.logError)).pipe(flatten()).pipe(rename(renameOptions)).pipe(gulp.dest(dstPath));
+	}
+
+	function jsMinify() {
+		return gulp.src([srcRoot + "scripts/**/*.js"]).pipe(jsMin()).pipe(rename(renameOptions)).pipe(flatten()).pipe(gulp.dest(dstPath));
+	}
+
+	function htmlMinify() {
+		return gulp.src([srcRoot + "templates/**/*.html"]).pipe(htmlMin({
+			collapseWhitespace: true,
+			removeComments: true
+		})).pipe(flatten()).pipe(gulp.dest(dstPath));
+	}
+
+	function watchHtml() {
+		var watcher = gulp.watch([srcRoot + "templates/**/*.html"]);
+		watcher.on("all", function(event, path, stats) {
+			util.log("Watching changes - File: " + path + " | Event: " + event);
+			htmlMinify();
+		});
+	}
+
+	function watchJs() {
+		var watcher = gulp.watch([srcRoot + "../*.js"]);
+		watcher.on("all", function() {
+			util.log("Watching changes - File: " + path + " | Event: " + event);
+			jsMinify();
+		});
+	}
+
+	function watchSass() {
+		var watcher = gulp.watch([srcRoot + "../*.scss"]);
+		watcher.on("all", function() {
+			util.log("Watching changes - File: " + path + " | Event: " + event);
+			sassCompileMinify();
+		});
+	}
+
+	function handleDependencies() {
+		var basePath = "node_modules/";
+		var js = gulp.src([
+			basePath + "angular/angular.min.js",
+			basePath + "angular/angular.min.js.map",
+			basePath + "angular-route/angular-route.min.js",
+			basePath + "angular-route/angular-route.min.js.map",
+			basePath + "jquery/dist/**/jquery.min.js",
+			basePath + "jquery/dist/**/jquery.min.js.map",
+			basePath + "bootstrap/dist/js/bootstrap.min.js",
+		]).pipe(flatten()).pipe(gulp.dest(dstPath));
+		var css = gulp.src([
+			basePath + "bootstrap/dist/css/bootstrap.min.css",
+			basePath + "bootstrap/dist/css/bootstrap.min.css.map",
+		]).pipe(replace("../fonts/", "")).pipe(gulp.dest(dstPath));
+		var fonts = gulp.src([basePath + "bootstrap/dist/fonts/*"]).pipe(flatten()).pipe(gulp.dest(dstPath));
+
+		return merge(css, fonts, js);
+	}
+
+	function run() {
+		return gulp.src(dstPath).pipe(shell("http-server -p 3000 " + dstPath));
+	}
+	gulp.task("build", gulp.series(clean, gulp.parallel(handleDependencies, sassCompileMinify, jsMinify, htmlMinify)));
+	gulp.task("watch", gulp.parallel(watchHtml, watchJs, watchSass));
+	gulp.task("start", gulp.series("build", gulp.parallel("watch", run)));
+})();
